2449c3ae8e69b4b59a230417f4facdd931ba7f0d
esdmr <esdmr0@gmail.com>
2021-09-06 00:08:44 +0430

    Update example for v1.1

--- examples/v1.1.ts
+++ examples/v1.1.ts
@@ -1,6 +1,21 @@
 import * as assert from '@esdmr/assert';
 
-class SpecialObject {}
-const array: unknown = new SpecialObject();
+export function parseConfig (json: unknown) {
+	try {
+		assert.isObject(json);
+		assert.isNotNull(json);
+		assert.isBoolean(json.private, 'property "private"');
+		assert.isEqual(json.private, false, 'property "private"');
+		assert.isNumber(json.major, 'property "major"');
+		assert.isGreater(json.major, 0, 'property "major"');
+		assert.isNumber(json.minor, 'property "minor"');
+		assert.isGreaterOrEqual(json.minor, 0, 'property "minor"');
+	} catch (error) {
+		throw assert.wrap(error, 'Failed to parse config');
+	}
 
-assert.isInstanceOf(array, SpecialObject);
+	return {
+		major: json.major,
+		minor: json.minor,
+	};
+}
