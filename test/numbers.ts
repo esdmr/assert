6fa10e4b8f7d2b66394e3b91cdff1249eb6fa16e
esdmr <esdmr0@gmail.com>
2022-07-11 22:50:30 +0430

    Fix CI

--- test/numbers.ts
+++ test/numbers.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { test } from 'tap';
 import { testDetail } from '#test/test-util/format.js';
 import * as numbers from '#src/numbers.js';
